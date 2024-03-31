79439f00f82793e9274bb2c009419a2b3a28f8bf
kgrace61 <110522366+kgrace61@users.noreply.github.com>
2024-03-31 01:18:08 -0500

    lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    
+    fib_sequence = [0,1]
+    
+    while len(fib_sequence) < length:
+        next_number = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_number)
+        
+    print(fib_sequence[:length])
